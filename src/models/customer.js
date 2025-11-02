6f4514e6a5a67dbd6335fb7623d406f74391b36d
christopher18-cyber <christopheradegoke4@gmail.com>
2025-11-02 14:56:37 +0100

    did error, login and register controllers

--- src/models/customer.js
+++ src/models/customer.js
@@ -1,82 +1,92 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "../config/db.js";
 
-const Customer = sequelize.define("Customer", {
-id: {
-type: DataTypes.UUID,
-defaultValue: DataTypes.UUIDV4,
-primaryKey: true,
-autoIncrement: true,
-},
+const Customer = sequelize.define(
+  "Customer",
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
 
-firstName: {
-type: DataTypes.STRING(50),
-allowNull: false,
-},
+    firstName: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
 
-lastName: {
-type: DataTypes.STRING(50),
-allowNull: false,
-},
+    lastName: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    fullName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
 
-email: {
-type: DataTypes.STRING(100),
-allowNull: false,
-unique: true,
-validate: {
-isEmail: true,
-},
-},
+    email: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
 
-phone: {
-type: DataTypes.STRING(20),
-allowNull: false,
-unique: true,
-},
+    phone: {
+      type: DataTypes.STRING(20),
+      allowNull: false,
+      unique: true,
+    },
 
-address: {
-type: DataTypes.STRING,
-allowNull: true,
-},
+    address: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
 
-city: {
-type: DataTypes.STRING,
-allowNull: true,
-},
+    city: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
 
-state: {
-type: DataTypes.STRING,
-allowNull: true,
-},
+    state: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
 
-country: {
-type: DataTypes.STRING,
-allowNull: true,
-defaultValue: "Nigeria",
-},
+    country: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      defaultValue: "Nigeria",
+    },
 
-password: {
-type: DataTypes.STRING,
-allowNull: false,
-},
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
 
-role: {
-type: DataTypes.ENUM("customer", "admin"),
-defaultValue: "customer",
-},
+    role: {
+      type: DataTypes.ENUM("customer", "admin"),
+      defaultValue: "customer",
+    },
+    nin: {
+      type: DataTypes.STRING,
+    },
 
-accountStatus: {
-type: DataTypes.ENUM("active", "suspended", "deleted"),
-defaultValue: "active",
-},
+    accountStatus: {
+      type: DataTypes.ENUM("active", "suspended", "deleted"),
+      defaultValue: "active",
+    },
 
-profileImage: {
-type: DataTypes.STRING,
-allowNull: true,
-},
-}, {
-tableName: "Customers",
-timestamps: true,
-});
+    profileImage: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+  {
+    tableName: "Customers",
+    timestamps: true,
+  }
+);
 
 export default Customer;
